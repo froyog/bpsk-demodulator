e5a174ef7cc4e4f243d3850cc4e6bf4bc29a7967
Tom <scarlet0345@gmail.com>
2018-11-28 23:54:11 +0800

    finish up generate label

--- generate_data.py
+++ generate_data.py
@@ -16,11 +16,30 @@ def bpsk_module(arr):
     return bpsk
 
 def generate_label():
-    arr = np.array([0, 0, 1, 0, 0, 0, 1])
+    arr = np.array([0, 1, 0, 1])
     # looking for phase shifting
-    phase_shift_arr = np.zeros(len(arr), dtype=np.float32)
+    phase_shift_arr = np.zeros(len(arr))
     for i in range(1, len(arr)):
         phase_shift_arr[i] = 0 if arr[i] == arr[i-1] else 1
+    print(phase_shift_arr)
+    sampling_arr = np.arange(0, len(arr), 0.01)
+    label_arr = np.zeros(len(sampling_arr))
+    for j in range(len(sampling_arr)):
+        ele = sampling_arr[j]
+        if ele < 1:
+            label_arr[j] = 0
+            continue
+        if ele > round(ele) - 0.2 and ele < round(ele) + 0.2:
+            label_arr[j] = phase_shift_arr[int(round(ele)) - 1]
+            continue
+        label_arr[j] = 0
+    print(label_arr[0:100])
+    print(label_arr[100:200])
+    print(label_arr[200:300])
+    print(label_arr[300:400])
+
+
+generate_label()
 
 def main():
     # random signal size
