e619aca31fdad6dc38256dde40a2385eed4a2903
Tom <scarlet0345@gmail.com>
2018-12-04 22:20:56 +0800

    add agwn

--- generate_data.py
+++ generate_data.py
@@ -2,6 +2,13 @@ import numpy as np
 from math import pi, floor
 
 
+def add_awgn(signal, snr):
+    # snr: dB
+    snr = 10 ** (snr / 10.0)
+    xpower = np.sum(signal ** 2) / len(signal)
+    npower = xpower / snr
+    return np.random.randn(len(signal)) * np.sqrt(npower) + signal
+
 def bpsk_module(arr):
     size = len(arr)
     sampling_arr = np.arange(0, size, 0.01)
@@ -15,8 +22,7 @@ def bpsk_module(arr):
     bpsk = np.cos(coherent_carrier + pi * (sampled_arr - 1) + pi / 4)
     return bpsk
 
-def generate_label():
-    arr = np.array([0, 1, 0, 1])
+def generate_label(arr):
     # looking for phase shifting
     phase_shift_arr = np.zeros(len(arr))
     for i in range(0, len(arr) - 1):
@@ -36,10 +42,10 @@ def main():
     # random signal size
     ran_size = np.random.randint(16, 64)
     arr = np.random.randint(0, 2, ran_size)
-    # data_signal = bpsk_module(arr)
-    # data_label = generate_label(arr)
+    data_signal = bpsk_module(arr)
+    data_signal = add_awgn(data_signal, 2)
+    data_label = generate_label(arr)
 
-    pass
-
+main()
 # if __name__ == "__main__":
 #     main()
