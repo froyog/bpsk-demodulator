4cfb95add0890265ccd1c2cf2e1e0e2844698f44
Tom <scarlet0345@gmail.com>
2018-12-05 20:16:40 +0800

    update cnn network

--- demodulator_cnn.py
+++ demodulator_cnn.py
@@ -5,17 +5,19 @@ import keras
 def create_model(input_size):
     carrier_input = keras.layers.Input(shape=(1, input_size+1))
     first_cnn = keras.layers.Conv1D(input_size+1,
-                                    padding="same",
-                                    activation="relu",
-                                    kernel_size=8)(carrier_input)
+                                    padding='same',
+                                    activation='relu',
+                                    kernel_size=input_size+1)(carrier_input)
     second_cnn = keras.layers.Conv1D(input_size+1,
-                                    padding="same",
-                                    activation="relu",
-                                    kernel_size=8)(carrier_input)
+                                     padding='same',
+                                     activation='relu',
+                                     kernel_size=input_size+1)(carrier_input)
     merged_cnn = keras.layers.concatenate([first_cnn, second_cnn])
-    merged_cnn = keras.layers.Dense(2 * input_size + 4, activation="relu")(merged_cnn)
-    phase_shift = keras.layers.Dense(1, activation="relu")(merged_cnn)
+    merged_cnn = keras.layers.Dense(2 * input_size + 4, activation='relu')(merged_cnn)
+    phase_shift = keras.layers.Dense(1, activation='sigmoid')(merged_cnn)
     model = keras.models.Model(inputs=carrier_input, outputs=phase_shift)
-    model.summary()
+    
+    model.compile(optimizer='adam',
+                  loss='binary_crossentropy')
     
     return model
