01a4133244bd402fa175de301609e20f860c3f25
Tom <scarlet0345@gmail.com>
2018-12-05 23:03:36 +0800

    re-construnct cnn to apply data

--- demodulator_cnn.py
+++ demodulator_cnn.py
@@ -1,23 +1,25 @@
 import tensorflow as tf
-import keras
-
+from keras.models import Model
+from keras.layers import Conv1D, concatenate, Dense, Input, Flatten
 
 def create_model(input_size):
-    carrier_input = keras.layers.Input(shape=(1, input_size+1))
-    first_cnn = keras.layers.Conv1D(input_size+1,
-                                    padding='same',
-                                    activation='relu',
-                                    kernel_size=input_size+1)(carrier_input)
-    second_cnn = keras.layers.Conv1D(input_size+1,
-                                     padding='same',
-                                     activation='relu',
-                                     kernel_size=input_size+1)(carrier_input)
-    merged_cnn = keras.layers.concatenate([first_cnn, second_cnn])
-    merged_cnn = keras.layers.Dense(2 * input_size + 4, activation='relu')(merged_cnn)
-    phase_shift = keras.layers.Dense(1, activation='sigmoid')(merged_cnn)
-    model = keras.models.Model(inputs=carrier_input, outputs=phase_shift)
-    
+    carrier_input = Input(shape=(21, 1))
+    first_cnn = Conv1D(1,
+                       padding='same',
+                       activation='relu',
+                       kernel_size=input_size)(carrier_input)
+    second_cnn = Conv1D(1,
+                        padding='same',
+                        activation='relu',
+                        kernel_size=input_size)(carrier_input)
+    merged_cnn = concatenate([first_cnn, second_cnn])
+    flat = Flatten()(merged_cnn)
+    hidden = Dense(2 * input_size, activation='relu')(flat)
+    phase_shift = Dense(1, activation='sigmoid')(hidden)
+    model = Model(inputs=carrier_input, outputs=phase_shift)
+    model.summary()
     model.compile(optimizer='adam',
-                  loss='binary_crossentropy')
+                  loss='binary_crossentropy',
+                  metrics=['accuracy'])
     
     return model
--- train.py
+++ train.py
@@ -1,9 +1,42 @@
 import tensorflow as tf
 import keras
-
+import numpy as np
+import json
+import matplotlib.pyplot as plt
+from generate_data import generate
+from demodulator_cnn import create_model
 
 def preprocess():
-    pass
+    signal, (label_1, label_2) = generate()
+    input_train = np.empty((len(signal) - 20, 21))
+    output_1_train = np.empty(len(signal) - 20)
+    output_2_train = np.empty(len(signal) - 20)
+    for i in range(len(signal) - 20):
+        input_train[i] = np.array(signal[i: i + 21])
+        output_1_train[i] = label_1[i]
+        output_2_train[i] = label_2[i]
+    return (input_train, output_1_train, output_2_train)
 
 def train():
-    pass
+    input_train, output_1_train, output_2_train = preprocess()
+    model_1 = create_model(21)
+    input_val = input_train[:10000]
+    input_val = np.expand_dims(input_val, axis=2)
+    partial_input_train = input_train[10000:]
+    partial_input_train = np.expand_dims(partial_input_train, axis=2)
+    output_1_val = output_1_train[:10000]
+    partial_output_1_train = output_1_train[10000:]
+    history = model_1.fit(partial_input_train,
+                          partial_output_1_train,
+                          epochs=100,
+                          batch_size=512,
+                          validation_data=(input_val, output_1_val),
+                          verbose=1)
+    model_1.save('model/bpsk_dm.h5')
+    with open('history.json', 'w') as f:
+        json.dump(history.history, f)
+    # model_2 = create_model()
+
+
+if __name__ == '__main__':
+    train()
