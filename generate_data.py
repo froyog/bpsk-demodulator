b34f7d1cc0381a22acca219d86a23752454491d1
Tom <scarlet0345@gmail.com>
2018-12-05 15:23:49 +0800

    change algorithm to generate 2 label arrays

--- generate_data.py
+++ generate_data.py
@@ -1,5 +1,5 @@
 import numpy as np
-from math import pi, floor
+from math import pi, ceil, floor
 
 
 def add_awgn(signal, snr):
@@ -19,33 +19,38 @@ def bpsk_module(arr):
     fc = 2205
     freq = 20 * fc
     coherent_carrier = np.dot(2 * pi * fc, np.arange(0, (100 * size) / freq, 1 / freq))
-    bpsk = np.cos(coherent_carrier + pi * (sampled_arr - 1) + pi / 4)
+    bpsk = np.cos(coherent_carrier + pi * (sampled_arr - 1))
     return bpsk
 
 def generate_label(arr):
-    # looking for phase shifting
-    phase_shift_arr = np.zeros(len(arr))
-    for i in range(0, len(arr) - 1):
-        phase_shift_arr[i] = 0 if arr[i] == arr[i+1] else 1
     sampling_arr = np.arange(0, len(arr), 0.01)
-    label_arr = np.zeros(len(sampling_arr))
-    for j in range(len(sampling_arr)):
-        ele = sampling_arr[j]
-        if ele > round(ele) - 0.2 and ele < round(ele):
-            # include all 20 points before phase shifting
-            label_arr[j] = phase_shift_arr[int(round(ele)) - 1]
-            continue
-        label_arr[j] = 0
-    return label_arr
+    # label_arr_1 stands for phase shifting from 0 to pi
+    # label_arr_2 for pi to 0
+    label_arr_1 = np.zeros(len(sampling_arr))
+    label_arr_2 = np.zeros(len(sampling_arr))
+    # we don't care the last 100 points since phase shifting
+    # will never happen within
+    for i in range(len(sampling_arr) - 100):
+        ele = sampling_arr[i]
+        if ele > ceil(ele) - 0.21 and ele < ceil(ele):
+            # includes 21 points before phase shifting
+            data_i = int(ceil(ele))
+            if arr[data_i] == 0 and arr[data_i - 1] == 1:
+                # 1 -> 0, phase 0 -> pi
+                label_arr_1[i] = 1
+                continue
+            if arr[data_i] == 1 and arr[data_i - 1] == 0:
+                # 0 -> 1, phase pi -> 0
+                label_arr_2[i] = 1
+                continue
+        label_arr_1[i] = 0
+        label_arr_2[i] = 0
+    return label_arr_1, label_arr_2
 
-def main():
+def generate():
     # random signal size
     ran_size = np.random.randint(16, 64)
     arr = np.random.randint(0, 2, ran_size)
     data_signal = bpsk_module(arr)
     data_signal = add_awgn(data_signal, 2)
-    data_label = generate_label(arr)
-
-main()
-# if __name__ == "__main__":
-#     main()
+    return (data_signal, generate_label(arr))
